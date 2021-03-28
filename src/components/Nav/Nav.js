03275725e665e80f1a1982434e5492ed875ab0ff
ryand67 <rwdevenney@gmail.com>
2021-03-28 19:48:22 -0400

    nav

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../scss/Nav.scss';
+
+export default function Nav() {
+    return (
+        <nav className="nav">
+            <h1 className="nav-header">Lorem Architecture</h1>    
+            <ul className="nav-list">
+                <li className="nav-item">About</li>
+                <li className="nav-item">Work</li>
+                <li className="nav-item">Contact</li>
+            </ul>
+        </nav>
+    )
+}
