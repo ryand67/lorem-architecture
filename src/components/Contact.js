0dfc2837bcae853aca7487b37404dec2f2f072fc
ryand67 <rwdevenney@gmail.com>
2021-03-29 15:11:41 -0400

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,23 @@ import React from 'react';
 import '../scss/Contact.scss';
 
 export default function Contact() {
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        document.getElementById('contactForm').reset();
+    }
+
     return (
         <main className="contact-page">
             <h1 className="contact-header">Contact</h1>
-            <form className="contact-form">
+            <form onSubmit={(e) => handleFormSubmit(e)} className="contact-form" id="contactForm">
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" placeholder="Name..."/>
+                <input type="text" name="name" placeholder="Name..." id="formName" />
                 <label htmlFor="email">Email Address:</label>
-                <input type="text" name="email" placeholder="Email Address..." />
+                <input type="text" name="email" placeholder="Email Address..." id="formEmail" />
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" id="" cols="30" rows="10" placeholder="Message..."></textarea>
-                <button type="submit">Send Message</button>
+                <textarea name="message" id="formMessage" cols="30" rows="10" placeholder="Message..."></textarea>
+                <button type="submit" id="submitButton">Send Message</button>
             </form>
         </main>
     )
