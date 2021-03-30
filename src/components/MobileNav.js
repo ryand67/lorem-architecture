02f2b4408ce3fc52f5ac471000bd5de3a21b4aae
ryand67 <rwdevenney@gmail.com>
2021-03-30 16:14:09 -0400

    scroll to top

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 export default function MobileNav() {
     return (
         <nav className="nav">
-            <h1 className="nav-header"><Link to='/'>LA</Link></h1>
             <ul className="nav-list">
                 <li className="nav-item"><Link to='/'>Home</Link></li>
                 <li className="nav-item"><Link to='/about'>About</Link></li>
