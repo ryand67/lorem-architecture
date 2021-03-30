a6932d9a832fe6e4468c837d39df422f014b18ef
ryand67 <rwdevenney@gmail.com>
2021-03-30 16:56:15 -0400

    required inputs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,11 +25,11 @@ export default function Contact() {
             <h1 className="contact-header">Contact</h1>
             <form onSubmit={(e) => handleFormSubmit(e)} className="contact-form" id="contactForm">
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" placeholder="Name..." id="formName" />
+                <input type="text" name="name" placeholder="Name..." id="formName" required/>
                 <label htmlFor="email">Email Address:</label>
-                <input type="text" name="email" placeholder="Email Address..." id="formEmail" />
+                <input type="text" name="email" placeholder="Email Address..." id="formEmail" required/>
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" id="formMessage" cols="30" rows="20" placeholder="Message..."></textarea>
+                <textarea name="message" id="formMessage" cols="30" rows="20" placeholder="Message..." required></textarea>
                 <button type="submit" id="submitButton">Send Message</button>
             </form>
         </main>
